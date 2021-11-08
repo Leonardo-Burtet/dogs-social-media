029d26fa147390f4013bca5715a4e4243d3e803c
Leonardo Burtet <leonardoburtet@outlook.com>
2021-11-08 17:55:14 -0300

    :construction: Adicionando Pagina de Cadastro e automatizando login

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    message:
+      'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
+  },
 };
 
 const useForm = (type) => {
