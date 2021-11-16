fb636e8a4b5f0de2a2fe62bd87d5bf530e871e78
Leonardo Burtet <leonardoburtet@outlook.com>
2021-11-16 17:40:41 -0300

    :construction: Adicionando pagina individual para cada foto

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import UserContext from '../../UserContext';
+import { UserContext } from '../../UserContext';
 import Image from '../Helper/Image';
 import PhotoComments from './PhotoComments';
 import styles from './PhotoContent.module.css';
 import PhotoDelete from './PhotoDelete';
 
-const PhotoContent = ({ data }) => {
-  const { photo, comments } = data;
+const PhotoContent = ({ data, single }) => {
   const user = React.useContext(UserContext);
+  const { photo, comments } = data;
 
   return (
-    <div className={styles.photo}>
+    <div className={`${styles.photo} ${single ? styles.single : ''}`}>
       <div className={styles.img}>
         <Image src={photo.src} alt={photo.title} />
       </div>
@@ -35,7 +35,7 @@ const PhotoContent = ({ data }) => {
           </ul>
         </div>
       </div>
-      <PhotoComments id={photo.id} comments={comments} />
+      <PhotoComments single={single} id={photo.id} comments={comments} />
     </div>
   );
 };
--- src/api.js
+++ src/api.js
@@ -71,15 +71,17 @@ export function PHOTOS_GET({ page, total, user }) {
     },
   };
 }
+
 export function PHOTO_GET(id) {
   return {
-    url: ` ${API_URL}/api/photo/${id}`,
+    url: `${API_URL}/api/photo/${id}`,
     options: {
       method: 'GET',
       cache: 'no-store',
     },
   };
 }
+
 export function COMMENT_POST(id, body) {
   return {
     url: ` ${API_URL}/api/comment/${id}`,
