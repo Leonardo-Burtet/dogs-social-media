f46fa6a1190f8dcdc91055170d0194fde71ccbb9
Leonardo Burtet <leonardoburtet@outlook.com>
2021-11-17 16:48:15 -0300

    :construction: Adicionando Components Title e Footer

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -7,6 +7,7 @@ import useForm from '../../Hooks/useForm';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTO_POST } from '../../api';
 import { useNavigate } from 'react-router';
+import Head from '../Helper/Head';
 
 const UserPhotoPost = () => {
   const nome = useForm();
@@ -41,6 +42,7 @@ const UserPhotoPost = () => {
 
   return (
     <section className={`${styles.photoPost} animeLeft`}>
+      <Head title="Poste sua foto" />
       <form onSubmit={handleSubmit}>
         <Input label="Nome" type="text" name="nome" {...nome} />
         <Input label="Peso" type="number" name="peso" {...peso} />
