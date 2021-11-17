cba0f68c76e59e00f5a7418ebaa586a7c270d463
Leonardo Burtet <leonardoburtet@outlook.com>
2021-11-17 10:24:16 -0300

    :construction: Adicionando pagina do usuario

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import Photo from './Components/Photo/Photo';
 import User from './Components/User/User';
+import UserProfile from './Components/User/UserProfile';
 import { UserStorage } from './UserContext';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
             <Route path="login/*" element={<Login />} />
             <ProtectedRoute path="conta/*" element={<User />} />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
